45f8583178a60ffe6a9c4184352153110947dd8f
Sehmer Desktop <matheustae@hotmail.com>
2022-05-17 09:58:26 -0300

    Fineshed

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { AuthController } from "./controllers/AuthController";
 import { CheckoutDayController } from "./controllers/CheckoutDayController";
 
 const router = Router();
 
 const checkoutDayController = new CheckoutDayController();
-const authController = new AuthController();
 
 router.post("/api/checkoutDay", checkoutDayController.create);
 
@@ -19,6 +17,5 @@ router.put("/api/checkoutDay/:id", checkoutDayController.update);
 
 router.delete("/api/checkoutDay/:id", checkoutDayController.delete);
 
-router.post("/api/auth", authController.auth);
 
 export { router };
